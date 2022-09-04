16c617d98f6b7bec8c4dcd0e77cee2b16d832f80
JonathanAriass <UO283586@uniovi.es>
2022-09-04 12:37:51 +0200

    sign in page tweaks

--- components/Auth/AuthForm.js
+++ components/Auth/AuthForm.js
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import Image from "next/image";
 import Link from "next/link";
 
 import classes from "./AuthForm.module.css";
@@ -12,34 +13,43 @@ const AuthForm = () => {
     console.log("submit");
   };
 
+{/* <Image src="../../public/images/agendaIcon.jpg" layout="fill"/> */}
+
   return (
     <div className={classes.card}>
-      <h1>AGENDA</h1>
-
-      <form onSubmit={submitHandler}>
-        <div className={classes.inputblock}>
-          <label htmlFor="email">Your Email</label>
-          <input
-            id="email"
-            type="email"
-            required
-            ref={emailInputRef}
-            placeholder="email"
-          />
-        </div>
-        <div className={classes.inputblock}>
-          <label htmlFor="password">Your Password</label>
-          <input
-            id="password"
-            type="password"
-            required
-            ref={passwordInputRef}
-            placeholder="password"
-          />
-        </div>
-        <button>log in</button>
-      </form>
-      <Link href="/createacc">Create account</Link>
+      <div id={classes.test} className={classes.welcome}>
+        <h1>AGENDA</h1>
+        {/* <img src="images/agendaIcon.jpg" /> */}
+        <Image src="/agendaIcon.jpg" height="200" width="200"/>
+        <p>Make a todo list and start getting task into it!!</p>
+      </div>
+      <div className={classes.form}>
+      <h1>SIGN IN</h1>
+        <form onSubmit={submitHandler}>
+          <div className={classes.inputblock}>
+            <label htmlFor="email">Your Email</label>
+            <input
+              id="email"
+              type="email"
+              
+              ref={emailInputRef}
+              placeholder="email"
+            />
+          </div>
+          <div className={classes.inputblock}>
+            <label htmlFor="password">Your Password</label>
+            <input
+              id="password"
+              type="password"
+              required
+              ref={passwordInputRef}
+              placeholder="password"
+            />
+          </div>
+          <button>log in</button>
+        </form>
+        <Link href="/createacc">Create account</Link>
+      </div>
     </div>
   );
 };
